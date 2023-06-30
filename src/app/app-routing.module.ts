74833bfefa6664bd8e011b9e19d1877214fa818d
Jose Hernandez <joselel2011@gmail.com>
2023-06-29 20:53:04 -0600

    configuración de módulos de aplicación

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,60 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () => import('./authetication/authetication.module').then( m => m.AutheticationModule)
+  },
+  {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
+    path: 'auth',
+    loadChildren: () => import('./authetication/authetication.module').then( m => m.AutheticationModule)
+  },
+  {
+    path: 'inventory',
+    loadChildren: () => import('./authetication/authetication.module').then( m => m.AutheticationModule)
+  },
+  {
+    path: 'categories-products',
+    loadChildren: () => import('./authetication/authetication.module').then( m => m.AutheticationModule)
+  },
+  {
+    path: 'sales',
+    loadChildren: () => import('./authetication/authetication.module').then( m => m.AutheticationModule)
+  },
+  {
+    path: 'shopping',
+    loadChildren: () => import('./authetication/authetication.module').then( m => m.AutheticationModule)
+  },
+    {
+    path: 'roles-users',
+    loadChildren: () => import('./authetication/authetication.module').then( m => m.AutheticationModule)
+  },
+  {
+    path: 'statistics',
+    loadChildren: () => import('./authetication/authetication.module').then( m => m.AutheticationModule)
+  },
+  {
+    path: 'products',
+    loadChildren: () => import('./authetication/authetication.module').then( m => m.AutheticationModule)
+  },
+  {
+    path: 'cancellations',
+    loadChildren: () => import('./authetication/authetication.module').then( m => m.AutheticationModule)
+  },
+  // {
+  //   path: 'sales',
+  //   loadChildren: () => import('./authetication/authetication.module').then( m => m.AutheticationModule)
+  // },
+  // {
+  //   path: 'sales',
+  //   loadChildren: () => import('./authetication/authetication.module').then( m => m.AutheticationModule)
+  // },
+  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: '',
     pathMatch: 'full'
   },
 ];
---
+++ src/app/authetication/authetication.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AutheticationComponent } from './authetication.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [AutheticationComponent]
+})
+export class AutheticationModule { }
