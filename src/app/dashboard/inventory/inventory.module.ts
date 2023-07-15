c11d9aa8dbaf98ad5aba2109f3ddd224a99e4901
José Hernández <joselel2011@gmail.com>
2023-07-14 21:30:07 -0600

    configuracion de rutas inventory

--- src/app/dashboard/inventory/inventory.module.ts
+++ src/app/dashboard/inventory/inventory.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InventoryRoutingModule } from './inventory-routing.module';
-
+import { HistoryInventoryComponent } from './components/history-inventory/history-inventory.component';
+import { StockComponent } from './components/stock/stock.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HistoryInventoryComponent,
+    StockComponent
+  ],
   imports: [
     CommonModule,
     InventoryRoutingModule
