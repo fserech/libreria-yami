5bc5d50cefe273b6000223605813b0f1f72bd844
Fredy Serech <you@example.com>
2023-07-14 21:48:45 -0600

    componentes de productos agregados

--- src/app/dashboard/products/products.module.ts
+++ src/app/dashboard/products/products.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductsRoutingModule } from './products-routing.module';
+import { NewProductComponent } from './components/new-product/new-product.component';
+import { ViewProductsComponent } from './components/view-products/view-products.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NewProductComponent,
+    ViewProductsComponent
+  ],
   imports: [
     CommonModule,
     ProductsRoutingModule
