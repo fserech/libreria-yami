fa2d3464a82ea549eabfbbd23f93a305dd3d30a7
José Hernández <joselel2011@gmail.com>
2023-08-03 16:17:45 -0600

    service role general

--- src/app/shared/services/roles/roles.service.ts
+++ src/app/shared/services/roles/roles.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RolesService {
 
-constructor() { }
+  constructor(private firestore: AngularFirestore) { }
+
+  getRol(id: string): Observable<any> {
+    return this.firestore.collection('roles').doc(id).valueChanges();
+  }
 
 }
