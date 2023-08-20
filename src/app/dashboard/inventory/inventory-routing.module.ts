5446be98c8082396be0e3945f619dcdecca59960
Fredy Serech <you@example.com>
2023-08-20 15:26:04 -0600

    correccion rutas categoria

--- src/app/dashboard/inventory/inventory-routing.module.ts
+++ src/app/dashboard/inventory/inventory-routing.module.ts
@@ -5,7 +5,7 @@ import { StockComponent } from './components/stock/stock.component';
 
 const routes: Routes = [
   {path: '', component: StockComponent},
-  {path: 'history-inventory', component: HistoryInventoryComponent},
+  {path: 'history', component: HistoryInventoryComponent},
   {path: 'stock', component: StockComponent}
 ];
 
