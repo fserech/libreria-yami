3b1e14c6e594e113b62e50b79f34ff58fe46044d
Fredy Serech <you@example.com>
2023-09-13 11:20:44 -0600

    mantenimiento a modulo insumos

--- src/app/dashboard/articles/components/new-article/new-article.component.ts
+++ src/app/dashboard/articles/components/new-article/new-article.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ARTICLES_COLLECTION_NAME, CATEGORIES_COLLECTION_NAME } from 'src/app/shared/constants/collections-name-firebase';
+import { REGEX_TEXT_WITHOUT_SPACES } from 'src/app/shared/constants/reguex';
 import { Article } from 'src/app/shared/models/article';
 import { DashboardService } from 'src/app/shared/services/dashboard/dashboard.service';
 import { ToastService } from 'src/app/shared/services/toast/toast.service';
@@ -11,27 +13,62 @@ import { ToastService } from 'src/app/shared/services/toast/toast.service';
   styleUrls: ['./new-article.component.scss'],
 })
 export class NewArticleComponent implements OnInit {
-  title: string = 'Nuevo Articulo';
+  title: string = 'Nuevo Insumo';
   articleForm: FormGroup;
   load: boolean = false;
   article: Article;
+  copied: boolean = false;
   valuesFirestore: string[] = [];
   keywords: string[] = [];
+  record: Article = null;
+  mode: string = 'view';
+  reguexText:RegExp = REGEX_TEXT_WITHOUT_SPACES;
 
   constructor(
     private formBuilder: FormBuilder,
     private toastService: ToastService,
     private dashboardService: DashboardService,
+    private route: ActivatedRoute,
+    private router: Router,
   ) {
-    this.articleForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      mark: ['', Validators.required],
-      categoryRef: ['', Validators.required],
-      keywords: [''],
-    });
+    const uid = this.route.snapshot.params['uid'];
+    this.mode = this.route.snapshot.params['mode'];
+    this.getFiles();
+    if(uid && this.mode !== 'new'){
+      this.load = true;
+      this.title = (this.mode === 'view') ? 'Visualizar Insumo' : 'Editar Insumo';
+      this.dashboardService.getDocumentByIdToPromise(CATEGORIES_COLLECTION_NAME, uid)
+      .then((response: Article) => {
+        this.record = response;
+        this.articleForm.controls['name'].setValue(this.record.name);
+        this.articleForm.controls['description'].setValue(this.record.description);
+        this.articleForm.controls['categoryRef'].setValue(this.record.categoryRef);
+        const indexToRemove = this.record.keywords.indexOf(this.record.name.toLowerCase());
+        if (indexToRemove !== -1) {
+          this.record.keywords.splice(indexToRemove, 1);
+        }
+        this.valuesFirestore = this.record.keywords;
+        this.load = false;
+      })
+      .catch((error: any) => {
+        console.log(error);
+        this.load = false;
+      });
+    }
+
+    
   }
 
   ngOnInit() {}
+
+  getFiles(){
+    this.articleForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      categoryRef: ['', Validators.required],
+      keywords: ['', Validators.pattern(this.reguexText)]
+    });
+  }
 
   chipsEvent(keywords: string[]) {
     this.keywords = keywords;
@@ -39,25 +76,69 @@ export class NewArticleComponent implements OnInit {
 
   submit() {
     this.load = true;
-    this.article = {
-      name: this.articleForm.controls['name'].value,
-      mark: this.articleForm.controls['mark'].value,
-      categoryRef: this.dashboardService.getDocumentReference(CATEGORIES_COLLECTION_NAME, this.articleForm.controls['categoryRef'].value),
-      keywords: this.keywords,
-    };
+     if(this.keywords.length > 0){
+      this.keywords.push(this.articleForm.controls['name'].value.toLowerCase());
 
-    this.dashboardService.saveDocument(ARTICLES_COLLECTION_NAME, this.article)
-      .then((res: any) => {
-        console.log(res);
-        this.articleForm.reset();
-        this.keywords = [];
-        this.load = false;
-      })
-      .catch((error: any) => {
-        console.log(error);
-        this.articleForm.reset();
-        this.keywords = [];
-        this.load = false;
-      });
+      this.record = {
+        name: this.articleForm.controls['name'].value.toLowerCase(),
+        description: this.articleForm.controls['description'].value,
+        categoryRef: this.dashboardService.getDocumentReference(CATEGORIES_COLLECTION_NAME, this.articleForm.controls['categoryRef'].value),
+        keywords: this.keywords
+      }
+      if(this.mode == 'new'){
+        this.dashboardService
+          .saveDocument(ARTICLES_COLLECTION_NAME,this.record)
+          .then(( response: any ) => {
+            this.reset();
+          })
+          .catch(( error: any ) => {
+            console.log(error)
+            this.reset();
+          });
+      }else{
+        const uid = this.route.snapshot.params['uid'];
+        this.dashboardService
+          .udpateDocument(uid, ARTICLES_COLLECTION_NAME  , this.record)
+          .then((response: any) => {
+            this.reset('/dashboard/articles/all');
+          })
+          .catch((error: any) => {
+            console.log(error);
+            this.reset('/dashboard/articles/all');
+          });
+      }
+    }
+
+   
+  }
+  reset(route?: string){
+    this.articleForm.reset();
+    this.keywords = [];
+    this.valuesFirestore = [];
+    this.load = false;
+    if(route){
+      this.router.navigate([route]);
+    }else{
+      this.router.navigate(['/dashboard/articles']);
+    }
+
+  }
+
+  copyToClipboard(text: string | undefined) {
+    if (text) {
+      const textArea = document.createElement('textarea');
+      textArea.value = text;
+      document.body.appendChild(textArea);
+      textArea.select();
+      textArea.setSelectionRange(0, 99999); // Para navegadores móviles
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+      this.copied = true;
+      this.toastService.success('se copio UID del registro');
+      setTimeout(() => {
+        this.copied = false;
+      }, 1000); // Puedes ajustar el tiempo en milisegundos según tus preferencias
+
+    }
   }
 }
--- src/app/dashboard/brands/brands-routing.module.ts
+++ src/app/dashboard/brands/brands-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddEditBrandComponent } from './components/add-edit-brand/add-edit-brand.component';
 import { ViewAllBrandComponent } from './components/view-all-brand/view-all-brand.component';
+import { ArticlesResolver } from './resolvers/articles.resolver';
 
 
 const routes: Routes = [
   {path: '',component: AddEditBrandComponent },
   {path: 'all',component: ViewAllBrandComponent },
-  {path: ':mode/:uid', component: AddEditBrandComponent },
+  {path: ':mode/:uid', component: AddEditBrandComponent, resolve: {categories: ArticlesResolver} },
   {path: ':mode', component: AddEditBrandComponent }
 ];
 
